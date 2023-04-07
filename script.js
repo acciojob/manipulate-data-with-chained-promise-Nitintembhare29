a21f408b81c44823ac7acd2417b9102aa4d7812c
Nitintembhare29 <115885821+Nitintembhare29@users.noreply.github.com>
2023-04-07 12:32:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,26 @@
 //your JS code here. If required.
-function getNumbers() {
+function manipulateArray() {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve([1, 2, 3, 4]);
     }, 3000);
-  });
-}
-
-function filterOutOddNumbers(numbers) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const filteredNumbers = numbers.filter((number) => number % 2 === 0);
-      resolve(filteredNumbers);
-    }, 1000);
-  });
-}
-
-function multiplyEvenNumbersByTwo(numbers) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const multipliedNumbers = numbers.map((number) => {
-        return number % 2 === 0 ? number * 2 : number;
-      });
-      resolve(multipliedNumbers);
-    }, 2000);
-  });
-}
-
-getNumbers()
-  .then(filterOutOddNumbers)
-  .then(multiplyEvenNumbersByTwo)
-  .then((result) => {
-    const outputDiv = document.getElementById("output");
-    outputDiv.innerText = result.join(", ");
   })
-  .catch((error) => {
-    console.error(error);
+  .then((arr) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const filteredArr = arr.filter((num) => num % 2 === 0);
+        document.querySelector('#output').textContent = filteredArr;
+        resolve(filteredArr);
+      }, 1000);
+    });
+  })
+  .then((arr) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const multipliedArr = arr.map((num) => num * 2);
+        document.querySelector('#output').textContent = multipliedArr;
+        resolve(multipliedArr);
+      }, 2000);
+    });
   });
+}
