d7ee90f33f21c9f103115a82cc5d1af7daa8c7a8
Nitintembhare29 <115885821+Nitintembhare29@users.noreply.github.com>
2023-04-07 12:28:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+function getNumbers() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([1, 2, 3, 4]);
+    }, 3000);
+  });
+}
+
+function filterOutOddNumbers(numbers) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const filteredNumbers = numbers.filter((number) => number % 2 === 0);
+      resolve(filteredNumbers);
+    }, 1000);
+  });
+}
+
+function multiplyEvenNumbersByTwo(numbers) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const multipliedNumbers = numbers.map((number) => {
+        return number % 2 === 0 ? number * 2 : number;
+      });
+      resolve(multipliedNumbers);
+    }, 2000);
+  });
+}
+
+getNumbers()
+  .then(filterOutOddNumbers)
+  .then(multiplyEvenNumbersByTwo)
+  .then((result) => {
+    const outputDiv = document.getElementById("output");
+    outputDiv.innerText = result.join(", ");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
